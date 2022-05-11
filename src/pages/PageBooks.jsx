b4746eb6df5f5dd69886aaa08f04107f34b8e0a5
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 11:21:53 +0200

    id out

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -18,7 +18,6 @@ export const PageBooks = () => {
 		const _books = [];
 		bookData.results.forEach((rawBook) => {
 			_books.push({
-				id: rawBook.id,
 				title: rawBook.title,
 				author: rawBook.authors[0] ? rawBook.authors[0].name : '',
 			});
@@ -44,7 +43,7 @@ export const PageBooks = () => {
 					return (
 						<div key={index} className="book">
 							<div className="title">
-								{book.title} ({book.id})
+								{book.title}
 							</div>
 							{book.author !== '' ? (
 								<div className="author">{book.author}</div>
