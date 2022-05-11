422511f0e90864a62ae43ea88a772b06f87e8d2b
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 10:24:17 +0200

    showing titles

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const booksUrl = 'https://gutendex.com/books/?search=berlin';
+
 export const PageBooks = () => {
+	const [bookData, setBookData] = useState({count: 0, results: []});
+
+	useEffect(() => {
+		(async () => {
+			setBookData((await axios.get(booksUrl)).data);	
+		})();
+	}, []);
+
 	return (
 		<>
 		<h2>Books</h2>	
-		<p>There are no books yet.</p>
+		<p>There are {bookData.count} books yet.</p>
+
+			{bookData.results.map((book, index) => {
+				return (
+					<div className="book">
+						<div className="title">{book.title}</div>
+					</div>
+				)
+		})}
 		</>
 	)
 }
