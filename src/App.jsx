36d8b2005b782b6865b0313f77e7f938c25aaaef
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 09:48:46 +0200

    nav works

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import './pages/PageWelcome';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
 import { PageAbout } from './pages/PageAbout';
+import { NavLink } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Personal Site</h1>
-			<hr/>
-			Welcome | Books | About
-			<hr/>
+			<hr />
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink> |{' '}
+				<NavLink to="/books">Books</NavLink> |{' '}
+				<NavLink to="/about">About</NavLink>
+			</nav>
+			<hr />
 			<PageWelcome />
-			<PageBooks/>
-			<PageAbout/>
+			<PageBooks />
+			<PageAbout />
 		</div>
 	);
 }
