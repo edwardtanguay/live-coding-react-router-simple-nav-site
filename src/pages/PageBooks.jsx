9f77357c88b7f0c58fad1d45227bc939d9af7b70
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 12:00:23 +0200

    search works

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const booksUrl = 'https://gutendex.com/books/?search=paris';
-
 export const PageBooks = () => {
 	const [books, setBooks] = useState([]);
-	const [searchText, setSearchText] = useState('');
+	const [searchText, setSearchText] = useState('berlin');
 
 	const buildBooksArray = (bookData) => {
 		const _books = [];
@@ -39,60 +37,83 @@ export const PageBooks = () => {
 				title: rawBook.title,
 				author: rawBook.authors[0] ? rawBook.authors[0].name : '',
 				readUrl: getReadUrl(rawBook),
-				imageUrl: getImageUrl(rawBook)
+				imageUrl: getImageUrl(rawBook),
 			});
 		});
 		setBooks(_books);
 	};
 
-	useEffect(() => {
+	const loadData = () => {
 		(async () => {
+			const booksUrl = `https://gutendex.com/books/?search=${searchText}`;
 			const bookData = (await axios.get(booksUrl)).data;
 			buildBooksArray(bookData);
 		})();
+	};
+
+	useEffect(() => {
+		loadData();
 	}, []);
 
 	const handleSearch = () => {
-		alert('handlesearch');
-	}
+		setBooks([]);
+		loadData();
+	};
 
 	return (
 		<>
 			<h2>Books</h2>
 			<div className="searchArea">
-				<input value={searchText} onChange={(e) => setSearchText(e.target.value)} type="text" /> <button onClick={handleSearch}>Search</button>
+				<input
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+					type="text"
+				/>{' '}
+				<button onClick={handleSearch}>Search</button>
 			</div>
-			<p>There are {books.length} books yet.</p>
 
-			<div className="books">
-				{books.map((book, index) => {
-					return (
-						<div key={index} className="book">
-							<div className="image">
-								<img src={book.imageUrl} alt="book" />
-							</div>
-							<div className="info">
-								<div className="title">
-									{book.readUrl !== '' ? (
-										<a href={book.readUrl} target="_blank">
-											{book.title}
-										</a>
-									) : (
-										<>{book.title}</>
-									)}
-								</div>
-								{book.author !== '' ? (
-									<div className="author">{book.author}</div>
-								) : (
-									<div className="authorNotFound">
-										(Autor nicht bekannt)
+			{books.length === 0 ? (
+				<div className="waiting">Loading...</div>
+			) : (
+				<>
+					<p>There are {books.length} books yet.</p>
+
+					<div className="books">
+						{books.map((book, index) => {
+							return (
+								<div key={index} className="book">
+									<div className="image">
+										<img src={book.imageUrl} alt="book" />
 									</div>
-								)}
-							</div>
-						</div>
-					);
-				})}
-			</div>
+									<div className="info">
+										<div className="title">
+											{book.readUrl !== '' ? (
+												<a
+													href={book.readUrl}
+													target="_blank"
+												>
+													{book.title}
+												</a>
+											) : (
+												<>{book.title}</>
+											)}
+										</div>
+										{book.author !== '' ? (
+											<div className="author">
+												{book.author}
+											</div>
+										) : (
+											<div className="authorNotFound">
+												(Autor nicht bekannt)
+											</div>
+										)}
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</>
+			)}
 		</>
 	);
 };
